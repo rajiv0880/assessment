79f850bb416cf4e5cd343aa302f0a4a6967f7341
Rajiv Joshi <rony.joshi008@gmail.com>
2024-02-20 16:32:00 +0530

    bootstrap

--- jquery assessment/strength.js
+++ jquery assessment/strength.js
@@ -148,8 +148,6 @@
         }
     };
 
-    // A really lightweight plugin wrapper around the constructor,
-    // preventing against multiple instantiations
     $.fn[pluginName] = function ( options ) {
         return this.each(function () {
             if (!$.data(this, "plugin_" + pluginName)) {
